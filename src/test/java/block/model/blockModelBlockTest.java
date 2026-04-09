d927ec44a90a2e98fd121fd7c3e886c1ca4888db
Clemens Beese <clemensbeese@dhcp-10-22-169-206.wlan.ntnu.no>
2026-04-09 12:17:14 +0200

    fikset compilation & lagt inn kommentarer på test

--- src/test/java/block/model/blockModelBlockTest.java
+++ src/test/java/block/model/blockModelBlockTest.java
@@ -9,30 +9,35 @@ public class blockModelBlockTest {
 
     @BeforeEach
     public void setUp() {
-        block = new Block(3);  // Opprett blokk på kolonne 3
+        block = new Block(3);
     }
+
 
     @Test
     public void testBlockInitialRow() {
-        assertEquals(0, block.getRow());  // Skal starte på rad 0
+        assertEquals(0, block.getRow());
     }
+    //sjekker at en ny blokk starter øverst
 
     @Test
     public void testBlockInitialCol() {
-        assertEquals(3, block.getCol());  // Skal være på kolonne 3
+        assertEquals(3, block.getCol());
     }
+    //sjekker om Block(3) faktisk starter på kolonne 3
 
     @Test
     public void testBlockMoveDown() {
         block.moveDown();
-        assertEquals(1, block.getRow());  // Skal bevege seg ned til rad 1
+        assertEquals(1, block.getRow());
     }
+    //sjekker om etter at vi kaller moveDown at den faktisk flyttes en rad ned,
 
     @Test
     public void testBlockMoveDownMultipleTimes() {
         block.moveDown();
         block.moveDown();
         block.moveDown();
-        assertEquals(3, block.getRow());  // Skal være på rad 3 etter 3 moveDown()
+        assertEquals(3, block.getRow()); 
     }
+    //sjekker samme som over bare 3 ganger/ flere ganger
 }
